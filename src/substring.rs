9046f987da3883bcfad85cc22211ddf7b485efeb
beerpsi <92439990+beer-psi@users.noreply.github.com>
2024-07-30 15:36:27 +0700

    idk what im doing

--- src/substring.rs
+++ src/substring.rs
@@ -2,15 +2,15 @@ use core::str::pattern::{Pattern, Searcher};
 
 pub trait Substring {
     /// Returns a substring before the first occurence of pattern.
-    fn substring_before<P: Pattern>(&self, pat: P) -> Option<&str>;
+    fn substring_before<P: Pattern>(&self, pat: P) -> &str;
 
     /// Returns a substring after the first occurence of pattern.
-    fn substring_after<P: Pattern>(&self, pat: P) -> Option<&str>;
+    fn substring_after<P: Pattern>(&self, pat: P) -> &str;
 }
 
 impl Substring for str {
     #[inline]
-    fn substring_before<P: Pattern>(&self, pat: P) -> Option<&str> {
+    fn substring_before<P: Pattern>(&self, pat: P) -> &str {
         match self.find(pat) {
             Some(i) => &self[..i],
             None => "",
@@ -18,7 +18,7 @@ impl Substring for str {
     }
 
     #[inline]
-    fn substring_after<P: Pattern>(&self, pat: P) -> Option<&str> {
+    fn substring_after<P: Pattern>(&self, pat: P) -> &str {
         match pat.into_searcher(self).next_match() {
             Some((_, end)) => &self[end..],
             None => "",
